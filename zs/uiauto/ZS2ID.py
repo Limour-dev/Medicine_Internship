75c9756b20dd09b4358dd7581fee6848ae037ce0
Limour <93720049+Limour-dev@users.noreply.github.com>
2025-04-02 22:44:20 +0800

    Add files via upload

--- zs/uiauto/ZS2ID.py
+++ zs/uiauto/ZS2ID.py
@@ -1,13 +1,17 @@
-a = '''
-'''.strip()
-b = '''
-'''.strip()
+try:
+    from clipboard import set_clipboard_text, get_clipboard_text
+except ModuleNotFoundError:
+    from tmp.clipboard import set_clipboard_text, get_clipboard_text
+
+a = get_clipboard_text()
+a = a.strip()
+
+input('任意键继续输入下一份数据')
+
+b = get_clipboard_text()
+b = b.strip()
 
 zs2id = {k.lower().strip():v for v,k in (x.split('\t', maxsplit=1) for x in a.split('\n'))}
 res = [zs2id.get(k.lower().strip(), 'NA') for k in b.split('\n')]
-try:
-    from clipboard import set_clipboard_text
-except ModuleNotFoundError:
-    from tmp.clipboard import set_clipboard_text
 
 set_clipboard_text('\n'.join(res))
