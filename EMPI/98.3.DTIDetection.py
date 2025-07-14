b1075615f174f54135c6dfa1d7099c44992966ad
Limour <93720049+Limour-dev@users.noreply.github.com>
2025-07-14 15:37:24 +0800

    Add files via upload

--- EMPI/98.3.DTIDetection.py
+++ EMPI/98.3.DTIDetection.py
@@ -85,3 +85,59 @@ for pths in zid.values():
         if os.path.exists(pth):
             print('unlink')
             os.unlink(pth)
+
+
+# DTI Ex
+if True:
+    def dti_ex_o(pth1):
+        with open(pth1, 'r', encoding='utf-8') as rf:
+            csv = rf.read()
+        csv = csv.splitlines()
+        csv = csv[3:10]
+        line = []
+        for o in csv:
+            o = o.strip().split(',')
+            if o[0] in {'FA', 'MD'}:
+                line.append(o[1])
+            else:
+                line.append(o[3])
+        return line
+    res = []
+    for zsid in sl:
+        tmp = zsid.split('\t')
+        zsid = tmp[0].strip().upper()
+        sst = datetime.strptime(tmp[-1], '%Y/%m/%d')
+        if zsid:
+            if zsid in zid:
+                data = zid[zsid]
+            else:
+                res.append('NA\tNA')
+                continue
+        else:
+            if res[-1].startswith('NA'):
+                res.append('NA\tNA')
+                continue
+        tmp = [(abs(sst - x[0]), x[0], x[1]) for x in data]
+        tmp.sort(key = lambda x:x[0])
+        tmp = tmp[0]
+        if tmp[0].total_seconds() > 3600*24*5:
+            res.append(datetime.strftime(tmp[1],'%Y/%m/%d') + '\tNA')
+            continue
+        gline = [datetime.strftime(tmp[1],'%Y/%m/%d')]
+        pth = os.path.join(tmp[-1], 'Python_post_processing', 'results', 'sectors_tables', 'results_table_sector_anterior.csv')
+        print(pth)
+        gline.extend(dti_ex_o(pth))
+        pth = os.path.join(tmp[-1], 'Python_post_processing', 'results', 'sectors_tables', 'results_table_sector_inferior.csv')
+        print(pth)
+        gline.extend(dti_ex_o(pth))
+        pth = os.path.join(tmp[-1], 'Python_post_processing', 'results', 'sectors_tables', 'results_table_sector_lateral.csv')
+        print(pth)
+        gline.extend(dti_ex_o(pth))
+        pth = os.path.join(tmp[-1], 'Python_post_processing', 'results', 'sectors_tables', 'results_table_sector_septal.csv')
+        print(pth)
+        gline.extend(dti_ex_o(pth))
+        pth = os.path.join(tmp[-1], 'Python_post_processing', 'results', 'results_table.csv')
+        print(pth)
+        gline.extend(dti_ex_o(pth))
+        res.append('\t'.join(gline))
+    pyperclip.copy('\n'.join(res))
