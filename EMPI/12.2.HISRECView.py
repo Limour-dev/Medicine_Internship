63f4d6bbf7a3095919bec917bd5aed584c634bec
Limour <93720049+Limour-dev@users.noreply.github.com>
2025-07-21 00:28:02 +0800

    Add files via upload

--- EMPI/12.2.HISRECView.py
+++ EMPI/12.2.HISRECView.py
@@ -18,17 +18,19 @@ if True:
     pto = input('请输入保存路径：')
     if not os.path.exists(pto):
         os.mkdir(pto)
+if True
+    class HisRec:
+        def __init__(self, _t, _n, _r):
+            self.time = _t
+            self.name = _n
+            self.rec = _r
+        def getText(self):
+            _t = self.time.strftime('%Y-%m-%d')
+            return f'{_t}\t{clear_value(self.name)}\n{clear_value(self.rec)}\n\n'
 
-class HisRec:
-    def __init__(self, _t, _n, _r):
-        self.time = _t
-        self.name = _n
-        self.rec = _r
-    def getText(self):
-        _t = self.time.strftime('%Y-%m-%d')
-        return f'{_t}\t{clear_value(self.name)}\n{clear_value(self.rec)}\n\n'
-
-reg_ipd_skip = re.compile(r'(须知)|(同意)|(自评)|(告知)|(清单)')
+    reg_ipd_skip = re.compile(r'(须知)|(同意)|(自评)|(告知)|(清单)')
+    reg_ipd_labs = re.compile(r'【主要化验结果】[^【]+')
+    
 for num,zid in enumerate(zs):
     pth1 = os.path.join(pts, zid)
     print(pth1)
@@ -36,6 +38,21 @@ for num,zid in enumerate(zs):
         print('skip')
         continue
     res = []
+
+    pth2 = os.path.join(pth1, 'ris')
+    pth3s = os.listdir(pth2)
+    for pth3 in pth3s:
+        pth3 = os.path.join(pth2, pth3)
+        print(pth3)
+        with open(pth3, 'r', encoding='utf-8') as rf:
+            d_s = BeautifulSoup(rf.read())
+        onet = d_s.select_one('#table8 > tr:nth-child(2) > td:nth-child(2)').getText().strip()
+        onet = datetime.strptime(onet, r'%Y-%m-%d %H:%M')
+        onen = d_s.select_one("#table8 > tr:nth-child(5) > td:nth-child(2)").getText().strip()
+        onen += '-' + d_s.select_one("#table8 > tr:nth-child(5) > td:nth-child(4)").getText().strip()
+        oner = d_s.select_one("#table8 > tr:nth-child(7) > td").getText().strip()
+        res.append(HisRec(onet, onen, oner))
+    
     pth2 = os.path.join(pth1, 'ipd')
     pth3s = os.listdir(pth2)
     for pth3 in pth3s:
@@ -54,6 +71,8 @@ for num,zid in enumerate(zs):
             onet = datetime.strptime(one['create_date'], r'%Y-%m-%d %H:%M:%S')
             d_s = BeautifulSoup(one['form'], "html.parser")
             oner = d_s.getText()
+            if onen.find('出院') >= 0:
+                oner = reg_ipd_labs.sub('', clear_value(oner))
             res.append(HisRec(onet, onen, oner))
 
     pth2 = os.path.join(pth1, 'opd')
@@ -83,6 +102,7 @@ for num,zid in enumerate(zs):
             onen = ndiv.select_one('table > tr > td:nth-child(2) > span > strong').getText()
             oner = rdiv.getText()
             res.append(HisRec(onet, onen, oner))
+    
     res.sort(key=lambda x:x.time)
     pth1 = os.path.join(pto, f'{num:03}_{zid}.txt')
     print(pth1)
