b400d60c91dc22bbab6ca12a963ff7219c840349
Limour <93720049+Limour-dev@users.noreply.github.com>
2025-09-13 00:17:52 +0800

    Add files via upload

--- EMPI/12.2.HISRECView.py
+++ EMPI/12.2.HISRECView.py
@@ -5,9 +5,11 @@ if True:
     from datetime import datetime
 
     reg_sp = re.compile(r'[\n\r]\s*')
+    reg_nn = re.compile(r'\n\n+')
     def clear_value(_v):
         _v = _v.replace('\t', ' ').replace('','')
         _v = reg_sp.sub('\n', _v)
+        _v = reg_nn.sub('\n', _v)
         return _v.strip()
     
     input('任意键粘贴ZSID...')
@@ -18,7 +20,7 @@ if True:
     pto = input('请输入保存路径：')
     if not os.path.exists(pto):
         os.mkdir(pto)
-if True
+if True:
     class HisRec:
         def __init__(self, _t, _n, _r):
             self.time = _t
@@ -30,7 +32,34 @@ if True
 
     reg_ipd_skip = re.compile(r'(须知)|(同意)|(自评)|(告知)|(清单)')
     reg_ipd_labs = re.compile(r'【主要化验结果】[^【]+')
-    
+
+skipPd = True
+
+def htd2ttb(table):
+    rows = []
+    for tr in table.find_all("tr"):
+        cells = tr.find_all(["td","th"])
+        row = [clear_value(cell.get_text(strip=True)) for cell in cells]
+        rows.append(row)
+    if not rows:
+        return ''
+    max_cols = max(len(row) for row in rows)
+    for i, row in enumerate(rows):
+        if len(row) < max_cols:
+            # 补齐空字符串
+            row.extend([""] * (max_cols - len(row)))
+    # 计算每列最大宽度，方便排版
+    col_widths = [max(len(row[i]) for row in rows) for i in range(len(rows[0]))]
+    lines = []
+    for row in rows:
+        formatted = "  ".join(word.ljust(col_widths[i]) for i, word in enumerate(row))
+        lines.append(formatted)
+
+    table_text = "\n".join(lines)
+    return table_text
+
+reg_risr = re.compile(r'(心超)|(心脏)|(冠脉)', re.IGNORECASE)
+
 for num,zid in enumerate(zs):
     pth1 = os.path.join(pts, zid)
     print(pth1)
@@ -42,69 +71,86 @@ for num,zid in enumerate(zs):
     pth2 = os.path.join(pth1, 'ris')
     pth3s = os.listdir(pth2)
     for pth3 in pth3s:
+        nmbfuck = pth3.split('_')
         pth3 = os.path.join(pth2, pth3)
         print(pth3)
         with open(pth3, 'r', encoding='utf-8') as rf:
             d_s = BeautifulSoup(rf.read())
-        onet = d_s.select_one('#table8 > tr:nth-child(2) > td:nth-child(2)').getText().strip()
-        onet = datetime.strptime(onet, r'%Y-%m-%d %H:%M')
-        onen = d_s.select_one("#table8 > tr:nth-child(5) > td:nth-child(2)").getText().strip()
-        onen += '-' + d_s.select_one("#table8 > tr:nth-child(5) > td:nth-child(4)").getText().strip()
-        oner = d_s.select_one("#table8 > tr:nth-child(7) > td").getText().strip()
+        try:
+            onet = d_s.select_one('#table8 > tr:nth-child(2) > td:nth-child(2)').getText().strip()
+            onet = datetime.strptime(onet, r'%Y-%m-%d %H:%M')
+            onen = d_s.select_one("#table8 > tr:nth-child(5) > td:nth-child(2)").getText().strip()
+            onen += '-' + d_s.select_one("#table8 > tr:nth-child(5) > td:nth-child(4)").getText().strip()
+            if reg_risr.search(onen):
+                oner = d_s.select_one("#table8 > tr:nth-child(6) > td")
+                oner = oner.getText().strip() + '\n'
+            else:
+                oner = ''
+            oner += d_s.select_one("#table8 > tr:nth-child(7) > td").getText().strip()
+            
+        except AttributeError:
+            oner = '\n'.join(htd2ttb(fuck) for fuck in d_s.select('table'))
+            try:
+                onet = nmbfuck[0]
+                onet = datetime.strptime(onet, r'%Y-%m-%d')
+                onen = nmbfuck[1]
+            except:
+                continue
         res.append(HisRec(onet, onen, oner))
-    
-    pth2 = os.path.join(pth1, 'ipd')
-    pth3s = os.listdir(pth2)
-    for pth3 in pth3s:
-        pth3 = os.path.join(pth2, pth3)
-        print(pth3)
-        with open(pth3, 'r', encoding='utf-8') as rf:
-            data = json.load(rf)
-        data = data['result']
-        if not data:
-            continue
-        for one in data:
-            onen = one['sectionName']
-            if reg_ipd_skip.search(onen):
-                continue
-            print(onen)
-            onet = datetime.strptime(one['create_date'], r'%Y-%m-%d %H:%M:%S')
-            d_s = BeautifulSoup(one['form'], "html.parser")
-            oner = d_s.getText()
-            if onen.find('出院') >= 0:
-                oner = reg_ipd_labs.sub('', clear_value(oner))
-            res.append(HisRec(onet, onen, oner))
 
-    pth2 = os.path.join(pth1, 'opd')
-    pth3s = os.listdir(pth2)
-    for pth3 in pth3s:
-        pth3 = os.path.join(pth2, pth3)
-        print(pth3)
-        with open(pth3, 'r', encoding='utf-8') as rf:
-            d_s = BeautifulSoup(rf.read())
-        data = d_s.select_one('#labelOutPatEmrDocAndOrder')
-        if not data or not data.getText().strip():
-            continue
-        div = data.select('body > div')
-        div = [x for x in div if x.attrs.get('align') != 'right']
-        i = 0
-        while i < len(div):
-            ndiv = div[i]
-            i += 1
-            if not ndiv.attrs.get('onclick'):
+    if not skipPd:
+        pth2 = os.path.join(pth1, 'ipd')
+        pth3s = os.listdir(pth2)
+        for pth3 in pth3s:
+            pth3 = os.path.join(pth2, pth3)
+            print(pth3)
+            with open(pth3, 'r', encoding='utf-8') as rf:
+                data = json.load(rf)
+            data = data['result']
+            if not data:
                 continue
-            rdiv = div[i]
-            if not rdiv.attrs.get('id', '').startswith('title'):
+            for one in data:
+                onen = one['sectionName']
+                if reg_ipd_skip.search(onen):
+                    continue
+                print(onen)
+                onet = datetime.strptime(one['create_date'], r'%Y-%m-%d %H:%M:%S')
+                d_s = BeautifulSoup(one['form'], "html.parser")
+                oner = d_s.getText()
+                if onen.find('出院') >= 0:
+                    oner = reg_ipd_labs.sub('', clear_value(oner))
+                res.append(HisRec(onet, onen, oner))
+
+        pth2 = os.path.join(pth1, 'opd')
+        pth3s = os.listdir(pth2)
+        for pth3 in pth3s:
+            pth3 = os.path.join(pth2, pth3)
+            print(pth3)
+            with open(pth3, 'r', encoding='utf-8') as rf:
+                d_s = BeautifulSoup(rf.read())
+            data = d_s.select_one('#labelOutPatEmrDocAndOrder')
+            if not data or not data.getText().strip():
                 continue
-            i += 1
-            onet = ndiv.select_one('table > tr > td:nth-child(1) > span > strong').getText()
-            onet = datetime.strptime(onet, r'+%Y-%m-%d %H:%M')
-            onen = ndiv.select_one('table > tr > td:nth-child(2) > span > strong').getText()
-            oner = rdiv.getText()
+            div = data.select('body > div')
+            div = [x for x in div if x.attrs.get('align') != 'right']
+            i = 0
+            while i < len(div):
+                ndiv = div[i]
+                i += 1
+                if not ndiv.attrs.get('onclick'):
+                    continue
+                rdiv = div[i]
+                if not rdiv.attrs.get('id', '').startswith('title'):
+                    continue
+                i += 1
+                onet = ndiv.select_one('table > tr > td:nth-child(1) > span > strong').getText()
+                onet = datetime.strptime(onet, r'+%Y-%m-%d %H:%M')
+                onen = ndiv.select_one('table > tr > td:nth-child(2) > span > strong').getText()
+                oner = rdiv.getText()
             res.append(HisRec(onet, onen, oner))
     
     res.sort(key=lambda x:x.time)
-    pth1 = os.path.join(pto, f'{num:03}_{zid}.txt')
+    pth1 = os.path.join(pto, f'{num:05}_{zid}.txt')
     print(pth1)
     with open(pth1, 'w', encoding='utf-8') as wf:
         for r in res:
