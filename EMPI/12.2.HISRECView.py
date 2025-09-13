26b6b07532e40f6c9bd1de4878e227758f0dc089
Limour <93720049+Limour-dev@users.noreply.github.com>
2025-09-14 02:21:37 +0800

    Add files via upload

--- EMPI/12.2.HISRECView.py
+++ EMPI/12.2.HISRECView.py
@@ -6,7 +6,10 @@ if True:
 
     reg_sp = re.compile(r'[\n\r]\s*')
     reg_nn = re.compile(r'\n\n+')
-    def clear_value(_v):
+    reg_html = re.compile('<.+?>')
+    def clear_value(_v, rmHt=False):
+        if rmHt:
+            _v = reg_html.sub('', _v)
         _v = _v.replace('\t', ' ').replace('','')
         _v = reg_sp.sub('\n', _v)
         _v = reg_nn.sub('\n', _v)
@@ -33,7 +36,6 @@ if True:
     reg_ipd_skip = re.compile(r'(须知)|(同意)|(自评)|(告知)|(清单)')
     reg_ipd_labs = re.compile(r'【主要化验结果】[^【]+')
 
-skipPd = True
 
 def htd2ttb(table):
     rows = []
@@ -58,9 +60,20 @@ def htd2ttb(table):
     table_text = "\n".join(lines)
     return table_text
 
-reg_risr = re.compile(r'(心超)|(心脏)|(冠脉)', re.IGNORECASE)
+def hrmTb(bsoj):
+    for table in bsoj.find_all("table"):
+        table.decompose()
+    return bsoj
 
-for num,zid in enumerate(zs):
+skipiPd = False
+skipoPd = True
+skipPis = False
+
+reg_risr = re.compile(r'(心超)|(心脏)|(冠状动脉)', re.IGNORECASE)
+skiprisr = True
+
+for num in range(210,len(zs)):
+    zid = zs[num]
     pth1 = os.path.join(pts, zid)
     print(pth1)
     if not os.path.exists(pth1):
@@ -81,7 +94,7 @@ for num,zid in enumerate(zs):
             onet = datetime.strptime(onet, r'%Y-%m-%d %H:%M')
             onen = d_s.select_one("#table8 > tr:nth-child(5) > td:nth-child(2)").getText().strip()
             onen += '-' + d_s.select_one("#table8 > tr:nth-child(5) > td:nth-child(4)").getText().strip()
-            if reg_risr.search(onen):
+            if (not skiprisr) and reg_risr.search(onen):
                 oner = d_s.select_one("#table8 > tr:nth-child(6) > td")
                 oner = oner.getText().strip() + '\n'
             else:
@@ -98,7 +111,7 @@ for num,zid in enumerate(zs):
                 continue
         res.append(HisRec(onet, onen, oner))
 
-    if not skipPd:
+    if not skipiPd:
         pth2 = os.path.join(pth1, 'ipd')
         pth3s = os.listdir(pth2)
         for pth3 in pth3s:
@@ -120,7 +133,7 @@ for num,zid in enumerate(zs):
                 if onen.find('出院') >= 0:
                     oner = reg_ipd_labs.sub('', clear_value(oner))
                 res.append(HisRec(onet, onen, oner))
-
+    if not skipoPd:
         pth2 = os.path.join(pth1, 'opd')
         pth3s = os.listdir(pth2)
         for pth3 in pth3s:
@@ -148,9 +161,56 @@ for num,zid in enumerate(zs):
                 onen = ndiv.select_one('table > tr > td:nth-child(2) > span > strong').getText()
                 oner = rdiv.getText()
             res.append(HisRec(onet, onen, oner))
+    if not skipPis:
+        pth2 = os.path.join(pth1, 'pis')
+        if os.path.exists(pth2):
+            pth3s = os.listdir(pth2)
+        else:
+            pth3s = []
+        for pth3 in pth3s:
+            pth3 = os.path.join(pth2, pth3)
+            print(pth3)
+            if pth3.endswith('.json'):
+                with open(pth3, 'r', encoding='utf-8') as rf:
+                    data = json.load(rf)
+                data = data['data']
+                try:
+                    oner = '\n'.join(x['diagnostic_opinion'] for x in data['all_report'])
+                except TypeError:
+                    continue
+                oner = clear_value(oner, rmHt=True)
+                tmp = data.get('gigantic_inspection')
+                if tmp:
+                    oner = tmp + '\n' + oner
+                onet = datetime.strptime(data['receive_at'], r'%Y-%m-%d %H:%M:%S')
+                onen = '病理诊断报告单'
+            else:
+                with open(pth3, 'r', encoding='utf-8') as rf:
+                    d_s = BeautifulSoup(rf.read())
+                try:
+                    onet = d_s.select_one("body > div > table:nth-child(4) > tr:nth-child(2) > td:nth-child(8)").getText(strip=True)
+                    onet = datetime.strptime(onet, r'%Y-%m-%d')
+                    onen = '病理诊断报告单-' + d_s.select_one("body > div > table:nth-child(4) > tr:nth-child(3) > td:nth-child(4)").getText(strip=True)
+                    oner = d_s.select_one("body > div > table:nth-child(6) > tr:nth-child(2) > td")
+                    oner = hrmTb(oner)
+                    oner = oner.getText(strip=True)
+                    oner = d_s.select_one("body > div > table:nth-child(6) > tr:nth-child(1) > td").getText(strip=True) + '\n' + oner
+                except AttributeError:
+                    try:
+                        onet = d_s.select_one("body > table:nth-child(3) > tr:nth-child(5) > td:nth-child(2) > p > span").getText(strip=True)
+                        onet = datetime.strptime(onet, r'%Y-%m-%d %H:%M')
+                        onen = '病理诊断报告单-' + d_s.select_one("body > table:nth-child(3) > tr:nth-child(4) > td:nth-child(2) > p > span").getText(strip=True)
+                        oner = d_s.select_one("body > table:nth-child(5) > tr:nth-child(4) > td:nth-child(2)").getText(strip=True)
+                        oner = d_s.select_one("body > table:nth-child(5) > tr:nth-child(2) > td:nth-child(2)").getText(strip=True) + '\n' + oner
+                    except AttributeError:
+                        onen = '病理诊断报告单'
+                        oner = d_s.getText(strip = True)
+                        onet = os.path.split(pth3)[-1].split('_')[0]
+                        onet = datetime.strptime(onet, r'%Y-%m-%d')
+            res.append(HisRec(onet, onen, oner))
     
     res.sort(key=lambda x:x.time)
-    pth1 = os.path.join(pto, f'{num:05}_{zid}.txt')
+    pth1 = os.path.join(pto, f'{num:03}_{zid}.txt')
     print(pth1)
     with open(pth1, 'w', encoding='utf-8') as wf:
         for r in res:
