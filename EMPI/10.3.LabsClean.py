ff12e872de54fa81c71885483c61e9230a8ff5fd
Limour <93720049+Limour-dev@users.noreply.github.com>
2026-01-08 23:12:23 +0800

    Add files via upload

--- EMPI/10.3.LabsClean.py
+++ EMPI/10.3.LabsClean.py
@@ -4,7 +4,7 @@ if True:
     from datetime import datetime
 
     def re_find(_l, _r):
-        r = re.compile(_r)
+        r = re.compile(_r, re.IGNORECASE)
         for i, _i in enumerate(_l):
             if r.search(_i):
                 return i
@@ -63,7 +63,7 @@ if True:
     h = nfs[0].split('\t')
     
     i_id = re_find(h, 'ID')
-    i_t = re_find(h, '时间')
+    i_t = re_find(h, '_date')
     
     for line in nfs[1:]:
         line = line.strip()
@@ -81,6 +81,8 @@ if True:
 def extract(key, new_name):
     res = []
     for _case in cases:
+        if _case.ID not in labs:
+            continue
         c_labs = labs[_case.ID]
         # print(_case.ID)
         _line = []
